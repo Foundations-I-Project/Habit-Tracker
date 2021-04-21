da090259dfde677cde875162ac5b2d51711d3975
Matthew Saber <mattsaber21@yahoo.com>
2021-04-20 17:20:33 -0700

    added to local-storage-utils

--- local-storage/local-storage.utils.js
+++ local-storage/local-storage.utils.js
@@ -1,5 +1,5 @@
 const USER = 'USER';
-const logged_IN = 'LOGGED_IN';
+const LOGGEDIN = 'LOGGED_IN';
 
 export function createUser(username, password) {
     const user = {
@@ -9,3 +9,68 @@ export function createUser(username, password) {
     };
     updatedUser(user);
 }
+
+export function getUser() {
+    const stringUser = localStorage.getItem(USER);
+    const user = JSON.parse(stringUser);
+    return user;
+}
+
+export function updateUser(user) {
+    const stringUser = JSON.stringify(user);
+    localStorage.setItem(USER, stringUser);
+}
+
+export function userExists(username) {
+    const userInLocalStorage = getUser();
+
+    if (!userInLocalStorage) return false;
+
+    return username === userInLocalStorage.username;
+}
+
+export function usernameAndPasswordMatch(username, password) {
+    const userInLocalStorage = getUser();
+
+    if (!userExists(username) || userInLocalStorage.password !== password)
+        return false;
+    return true;
+}
+
+export function login(username) {
+    localStorage.setItem(LOGGED_IN, username);
+
+}
+
+export function logout() {
+    localStorage.setItem(LOGGED_IN, '');
+}
+
+export function loginAndRedirect(username) {
+    login(username);
+    window.location = './user-dashboard/index.html';
+}
+
+export function addHabit(habit) {
+    const newHabit = {
+        habit: habit,
+        completed: false,
+    };
+
+    const user = getUser();
+    user.habit.push(newHabit);
+
+    updateUser(user);
+}
+
+export function completehabit(message) {
+    const user = getuser();
+
+    const matchingHabit = user.habit.find((habit) => message === habit.habit);
+
+    matchingHabit.completed = true;
+
+    updateUser(user);
+}
+
+
